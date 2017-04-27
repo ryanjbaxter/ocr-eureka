914fa4324300d9d1ef9c539173e73bb9c04bc2d6
Ryan Baxter <rbaxter@pivotal.io>
2017-04-27 15:42:00 -0400

    Updating versions

--- src/test/java/com/ryanjbaxter/spring/cloud/ocr/eureka/OcrEurekaApplicationTests.java
+++ src/test/java/com/ryanjbaxter/spring/cloud/ocr/eureka/OcrEurekaApplicationTests.java
@@ -2,13 +2,12 @@ package com.ryanjbaxter.spring.cloud.ocr.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = OcrEurekaApplication.class)
-@WebAppConfiguration
+@SpringBootTest
 public class OcrEurekaApplicationTests {
 
 	@Test
